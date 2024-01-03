beec1eafcece7a70f9b760e339025b709eeae665
Bigcroftltd <155540852+Bigcroftltd@users.noreply.github.com>
2024-01-03 13:59:56 +0100

    Add files via upload

---
+++ bigcroftjava.js
@@ -0,0 +1,176 @@
+document.querySelectorAll('.Chainlink').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f1').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f1 span').onclick = () =>{
+    document.querySelector('.f1').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Razor').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f2').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f2 span').onclick = () =>{
+    document.querySelector('.f2').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Spike').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f3').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f3 span').onclick = () =>{
+    document.querySelector('.f3').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Roller').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f4').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f4 span').onclick = () =>{
+    document.querySelector('.f4').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Clearvu').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f5').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f5 span').onclick = () =>{
+    document.querySelector('.f5').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Speed').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f6').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f6 span').onclick = () =>{
+    document.querySelector('.f6').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Palisade').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f7').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f7 span').onclick = () =>{
+    document.querySelector('.f7').style.display = 'none'
+  };
+
+  document.querySelectorAll('.BRC').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f8').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f8 span').onclick = () =>{
+    document.querySelector('.f8').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Rain').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f9').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f9 span').onclick = () =>{
+    document.querySelector('.f9').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Collapsible').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f10').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f10 span').onclick = () =>{
+    document.querySelector('.f10').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Temporary').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f11').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f11 span').onclick = () =>{
+    document.querySelector('.f11').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Panel').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f12').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f12 span').onclick = () =>{
+    document.querySelector('.f12').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Stainless').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f13').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f13 span').onclick = () =>{
+    document.querySelector('.f13').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Dapolon').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f14').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f14 span').onclick = () =>{
+    document.querySelector('.f14').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Dispensers').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f15').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f15 span').onclick = () =>{
+    document.querySelector('.f15').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Electric').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f16').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f16 span').onclick = () =>{
+    document.querySelector('.f16').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Safety').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f17').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f17 span').onclick = () =>{
+    document.querySelector('.f17').style.display = 'none'
+  };
+
+  document.querySelectorAll('.Barb').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.f18').style.display = 'flex'
+    }
+  })
+  document.querySelector('.f18 span').onclick = () =>{
+    document.querySelector('.f18').style.display = 'none'
+  };
+
+
+
+
+  document.querySelectorAll('.imgforit img').forEach(Image =>{
+    Image.onclick = () =>{
+        document.querySelector('.coverforslidebar1').style.display = 'flex'
+        document.querySelector('.imgforitslid img').src = Image.getAttribute('src');
+    }
+  })
+  document.querySelector('.coverforslidebar1 span').onclick = () =>{
+    document.querySelector('.coverforslidebar1').style.display = 'none'
+  }
+
+  
